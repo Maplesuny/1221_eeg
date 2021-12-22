ba54388320780fb1bce8cdc984da435a9cc31804
unknown <t37583@adcmuh.org.tw>
2021-12-22 17:31:13 +0800

    差status

--- src/javascript/Setoption.js
+++ src/javascript/Setoption.js
@@ -3,37 +3,46 @@ import { ref, onMounted, watch, onBeforeUnmount, onBeforeMount, reactive } from 
 import axios from 'axios'
 import { useQuasar } from 'quasar'
 import timeline from '../javascript/Timeline'
-import { sharpSignalCellularAlt, sharpCollectionsBookmark } from '@quasar/extras/material-icons-sharp'
-export default function Setoption (elementID, start, end, montage) {
+import { sharpSignalCellularAlt, sharpCollectionsBookmark, sharp5g } from '@quasar/extras/material-icons-sharp'
+import { matAllInbox } from '@quasar/extras/material-icons'
+export default function Setoption (elementID, montage) {
     //-----for Loading timer------
     const $q = useQuasar()
     let timer
     //----------------------------
     let title = []
     let xAxis = []
-    let series = []
     let grid = []
     let dataZoom = []
     let toolbox = []
     let brush = []
+    const default_color = ref("#3a3c42")
+    var myChart
+
+
     // get channel pre number
     const channel_number_arr = [];
+
     var rangeArray = [
         [],
     ]
 
+    // 控制dataZoom可以操控多個chanenl, format :[0,1,2,3....x]
     let count_channel = [];
-    var data
 
-    // 將資料合併到save_arr ，format : [time,value]  value只有10秒資料
-    let all_save_arr = []
-
-    function opiton_function () {
-        title.push({
-            text: 'Beijing AQI',
-            left: '1%'
+    // 連接axios，get data
+    async function axioss (url) {
+        return await axios.get(url).then((res) => {
+            let data = res.data
+            return data
+        }).catch((err) => {
+            alert('連線失敗', err)
         })
 
+    }
+
+    // 設定基本option
+    function opiton_function (start, end) {
         xAxis.push({
             type: "value",
             show: true,
@@ -54,25 +63,24 @@ export default function Setoption (elementID, start, end, montage) {
             max: end,
             interval: 1,
         })
-
         grid.push({
-            left: "4.5%",
+            left: "100px",
             right: '1%',
             containLabel: false
         })
-
-        dataZoom.push([
-            {
-                type: 'inside',
-                xAxisIndex: count_channel,
-                zoomOnMouseWheel: 'ctrl'
-            },
-            {
-                type: 'slider',
-                show: true,
-            }
-        ])
-
+        dataZoom.push(
+            [
+                {
+                    type: 'inside',
+                    xAxisIndex: count_channel,
+                    zoomOnMouseWheel: 'alt'
+                },
+                {
+                    type: 'slider',
+                    show: true,
+                }
+            ]
+        )
         toolbox.push({
             // right: 4,
             feature: {
@@ -83,8 +91,8 @@ export default function Setoption (elementID, start, end, montage) {
                 mytools: {
                     show: true,
                     title: "Clear",
-                    // icon: "M12,0A12,12 0 0,1 24,12A12,12 0 0,1 12,24A12,12 0 0,1 0,12A12,12 0 0,1 12,0M12,2A10,10 0 0,0 2,12C2,14.4 2.85,16.6 4.26,18.33L18.33,4.26C16.6,2.85 14.4,2 12,2M12,22A10,10 0 0,0 22,12C22,9.6 21.15,7.4 19.74,5.67L5.67,19.74C7.4,21.15 9.6,22 12,22Z",
-                    icon: sharpSignalCellularAlt,
+                    icon: "M12,0A12,12 0 0,1 24,12A12,12 0 0,1 12,24A12,12 0 0,1 0,12A12,12 0 0,1 12,0M12,2A10,10 0 0,0 2,12C2,14.4 2.85,16.6 4.26,18.33L18.33,4.26C16.6,2.85 14.4,2 12,2M12,22A10,10 0 0,0 22,12C22,9.6 21.15,7.4 19.74,5.67L5.67,19.74C7.4,21.15 9.6,22 12,22Z",
+                    // icon: matAllInbox,
                     onclick: function () {
                         // myChart.dispatchAction({
                         //     type: "restore",
@@ -130,98 +138,7 @@ export default function Setoption (elementID, start, end, montage) {
                 borderColor: 'rgba(220,20,57,0.8)',
             },
         })
-
-
     }
-
-    function setOption (url) {
-        const charDom = document.getElementById(elementID)
-        let aa = {
-            width: 1000 + 'px',
-            height: 700 + 'px'
-        }
-        let myChart = echarts.init(charDom, null, aa)
-        let getW = document.getElementById(elementID).clientWidth
-        let getH = document.getElementById(elementID).clientHeight
-        charDom.style.width = getW + 'px'
-        charDom.style.height = getH + 700 + 'px'
-        axios.get(url).then((res) => {
-            let data = res.data
-            let option = {
-                tooltip: {
-                    trigger: 'axis'
-                },
-                grid: {
-                    left: '5%',
-                    right: '%',
-                    bottom: '10%'
-                },
-                xAxis: {
-                    data: data.map(function (item) {
-                        return item[0];
-                    })
-                },
-                yAxis: {},
-                toolbox: {
-                    right: 5,
-                    feature: {
-                        dataZoom: {
-                            yAxisIndex: 'none'
-                        },
-                        restore: {},
-                        saveAsImage: {}
-                    }
-                },
-                dataZoom: [
-                    {
-                        startValue: '2014-06-01'
-                    },
-                    {
-                        type: 'inside'
-                    }
-                ],
-                series: {
-                    name: 'Beijing AQI',
-                    type: 'line',
-                    data: data.map(function (item) {
-                        return item[1];
-                    }),
-                    markLine: {
-                        silent: true,
-                        lineStyle: {
-                            color: '#333'
-                        },
-                        data: [
-                            {
-                                yAxis: 50
-                            },
-                            {
-                                yAxis: 100
-                            },
-                            {
-                                yAxis: 150
-                            },
-                            {
-                                yAxis: 200
-                            },
-                            {
-                                yAxis: 300
-                            }
-                        ]
-                    }
-                }
-            }
-            option && myChart.setOption(option);
-
-        }).catch((err) => {
-            alert('連線失敗，請檢察連線')
-            console.log(
-                'Error Message :', err
-            )
-        })
-
-    }
-
 
     // 取得channel 名稱
     let channel_array = ref([])
@@ -232,122 +149,283 @@ export default function Setoption (elementID, start, end, montage) {
         return arr
     }
 
-
     // 資料與秒數合併 ==> arr[秒數,資料]
-    function convert_sec (arr, end, number, idx, data) {
-        const base = end / number
-        const list_array = []
+    function convert_sec (arr, start, end, idx, data) {
+        // 總筆數
+        let total_number = end * 512
+        //將筆數傳換為秒數，每筆幾秒
+        let convert = end / total_number
+        let conver_arr = []
         let sum = 0
-        for (let i = 0; i < number; i++) {
-            sum = sum + base
-            list_array.push([sum, data[idx]['value'][i] - 400 * idx])
+        console.log('conver_sec', start, end)
+        for (let i = 0; i < total_number; i++) {
+            sum = sum + convert
+            if (sum > start) {
+                conver_arr.push([sum, data[idx]['value'][i] - 500 * idx])
+            } else {
+                // if start/end 10/40  , at least start > 10 ,then i will start count
+                i = 0
+            }
         }
-        arr.push(list_array)
+        arr.push(conver_arr)
+        // console.log('arr_length', arr[idx].length)
+    }
+
+    //dynamic_10sdata用
+    function convert_sec2 (arr, start_time, end_time, idx, data) {
+        // 總筆數
+        let total_number = end_time * 512
+        // 將筆數換為秒數 
+        let conver = end_time / total_number
+        let conver_arr = []
+        let sum = 0
+        for (let i = 0; i < total_number; i++) {
+            sum = sum + conver
+            conver_arr.push([sum, data[idx]['value'][i] - 500 * idx])
+        }
+        // 要扣掉的筆數,
+        let delete_number = start_time * 512
+        // console.log(delete_number)
+        if (delete_number === 0) {
+            arr.push(conver_arr)
+        } else {
+            for (let j = 0; j < delete_number; j++) {
+                // 從頭開始移除
+                // conver_arr.shift()
+                // 從後面移除
+                conver_arr.pop()
+            }
+            arr.push(conver_arr)
+        }
+
         return arr
     }
 
 
-    function A1_A2 (url) {
+    async function show_option (url, start, end) {
         const charDom = document.getElementById(elementID)
+        let series = []
         let aa = {
-            width: 1800 + 'px',
-            height: 600 + 'px'
+            width: 1600 + 'px',
+            height: 670 + 'px'
         }
-        let myChart = echarts.init(charDom, null, aa)
+        myChart = echarts.init(charDom, null, aa)
 
-        axios.get(url).then((res) => {
-            data = res.data
-            const default_color = ref("#3a3c42")
-            // Count Channel number [0,1,2,3,...16]
+        let data = await axioss(url)
+        console.log('收進來的data', data)
 
-            for (let i = 0; i < Object.keys(data).length; i++) {
-                count_channel.push(i);
-            }
+        // Count Channel number [0,1,2,3,...16]
 
-            channel_name_function(channel_array.value, data.length, data)
+        for (let i = 0; i < Object.keys(data).length; i++) {
+            count_channel.push(i);
+        }
 
-            // 將資料合併到save_arr ，format : [time,value] 
-            let save_arr = []
-            for (let i = 0; i < data.length; i++) {
-                convert_sec(save_arr, end, 512 * end, i, data);
-            }
+        channel_name_function(channel_array.value, data.length, data)
 
-            console.log(save_arr)
+        // 將資料合併到save_arr ，format : [time,value] 
+        let save_arr = []
+        for (let i = 0; i < data.length; i++) {
+            convert_sec(save_arr, start, end, i, data);
+        }
 
-            channel_array.value.forEach(function (egg_parameter, idx) {
-                // channel_array[idx] , idx = 0 , is F1-C3  ，這裡拆要change
-                let de = data.length - 1 - idx
-                let arr_split = channel_array.value[de].split("");
-                for (let l = 0; l < 3; l++) {
-                    let conver_number = Number(arr_split[l]);
-                    if (!isNaN(conver_number)) {
-                        channel_number_arr.push(conver_number);
-                        if (conver_number % 2 === 0) {
-                            default_color.value = "#1607ed";
-                        } else if (conver_number % 2 != 0) {
-                            default_color.value = "#ed070f";
-                        }
+        channel_array.value.forEach(function (egg_parameter, idx) {
+            // channel_array[idx] , idx = 0 , is F1-C3  ，這裡拆要change
+            let de = data.length - 1 - idx
+            let arr_split = channel_array.value[de].split("");
+            for (let l = 0; l < 3; l++) {
+                let conver_number = Number(arr_split[l]);
+                if (!isNaN(conver_number)) {
+                    channel_number_arr.push(conver_number);
+                    if (conver_number % 2 === 0) {
+                        default_color.value = "#1607ed";
+                    } else if (conver_number % 2 != 0) {
+                        default_color.value = "#ed070f";
                     }
                 }
-                if (idx === 0) {
-                    default_color.value = "#0c0d0c";
-                }
-                if (arr_split[1] == 'z') {
-                    default_color.value = "#0bb30b";
-                }
-                series.push({
-                    type: "line",
-                    name: egg_parameter,
-                    data: save_arr[de],
-                    symbol: "none",
-                    smoth: true,
-                    color: default_color.value,
-                });
-
-            })
-
-            opiton_function()
-
-            let option = {
-                animation: false,
-                xAxis: xAxis,
-                yAxis: {
-                    show: false,
-                    type: "value",
-                    scale: true,
-                    axisLabel: {
-                        show: true,
-                        showMinLabel: true,
-                        showMaxLabel: true,
-                    },
-                    // 網格線
-                    splitLine: {
-                        show: false,
-                    },
-                    min: 385 * (-1 * data.length),
-                    // min: -7315,
-                    max: 300,
-                },
-                series: series,
-                grid: grid,
-                dataZoom: dataZoom,
-                toolbox: toolbox,
-                brush: brush
             }
-            option && myChart.setOption(option);
+            if (idx === 0) {
+                default_color.value = "#0c0d0c";
+            }
+            if (arr_split[1] == 'z') {
+                default_color.value = "#0bb30b";
+            }
+            series.push({
+                type: "line",
+                name: egg_parameter,
+                data: save_arr[de],
+                symbol: "none",
+                smoth: true,
+                color: default_color.value,
+            });
 
-            timeline(1, 1, rangeArray)
-
-        }).catch((err) => {
-            alert('連線失敗，請檢察連線')
-            console.log(
-                'Error Message :', err
-            )
         })
+
+        opiton_function(start, end)
+        let option = {
+            animation: false,
+            xAxis: xAxis,
+            yAxis: {
+                show: false,
+                type: "value",
+                scale: true,
+                axisLabel: {
+                    show: true,
+                    showMinLabel: true,
+                    showMaxLabel: true,
+                },
+                // 網格線
+                splitLine: {
+                    show: false,
+                },
+                min: 495 * (-1 * data.length),
+                // min: -7315,
+                max: 300,
+            },
+            series: series,
+            grid: grid,
+            dataZoom: [
+                {
+                    type: 'inside',
+                    xAxisIndex: count_channel,
+                    zoomOnMouseWheel: 'alt'
+                },
+                {
+                    type: 'slider',
+                    show: true,
+                }
+            ],
+
+            toolbox: toolbox,
+            brush: brush
+        }
+        option && myChart.setOption(option);
+
+        timeline(1, 1, rangeArray)
+
+    }
+
+    async function dynamic_10sdata (page, montage_type) {
+        // console.log('pageis', page)
+        let dynamic_start = (page * 10) - 10
+        let dynamic_end = page * 10
+        console.log('dynamic', dynamic_start, dynamic_end)
+        let url =
+            "http://127.0.0.1:8000/api/v1/eegData?start_time=" +
+            dynamic_start +
+            "&end_time=" +
+            dynamic_end +
+            "&montage_type=" +
+            montage_type;
+        let series1 = []
+        let Data = await axioss(url)
+        // console.log(Data)
+        // 將資料合併到save_arr ，format : [time,value]  value只有10秒資料
+        let all_save_arr = []
+        for (let i = 0; i < Data.length; i++) {
+            // convert_sec(all_save_arr, dynamic_start, dynamic_end, i, Data);
+            convert_sec2(all_save_arr, dynamic_start, dynamic_end, i, Data)
+        }
+
+        console.log(all_save_arr)
+
+        let next_page_value = 0
+        channel_array.value.forEach(function (egg_parameter, idx) {
+
+            let de = Data.length - 1 - idx
+            // console.log('Daa', all_save_arr[de])
+            let arr_split = channel_array.value[de].split("");
+            for (let l = 0; l < 3; l++) {
+                let conver_number = Number(arr_split[l]);
+                if (!isNaN(conver_number)) {
+                    channel_number_arr.push(conver_number);
+                    if (conver_number % 2 === 0) {
+                        default_color.value = "#1607ed";
+                    } else if (conver_number % 2 != 0) {
+                        default_color.value = "#ed070f";
+                    }
+                }
+            }
+            if (idx === 0) {
+                default_color.value = "#0c0d0c";
+            }
+            if (arr_split[1] == 'z') {
+                default_color.value = "#0bb30b";
+            }
+            series1.push({
+                type: "line",
+                name: egg_parameter,
+                data: all_save_arr[de],
+                symbol: "none",
+                smoth: true,
+                color: default_color.value,
+            });
+        })
+        let option = {
+            animation: false,
+            xAxis: {
+                type: "value",
+                show: true,
+                splitLine: {
+                    // 坐標軸分隔線
+                    show: true,
+                    lineStyle: {
+                        color: ["#626366"],
+                        width: 0.5,
+                        type: "solid",
+                        join: "miter",
+                    },
+                },
+                axisLabel: {
+                    formatter: function (value) {
+                        // 從start time抓開頭，從開頭+value
+                        next_page_value = dynamic_start
+                        next_page_value = next_page_value + value
+                        return next_page_value
+                    },
+                    show: true,
+                },
+                min: 0,
+                max: 10,
+                interval: 1,
+            },
+            yAxis: {
+                show: false,
+                type: "value",
+                scale: true,
+                axisLabel: {
+                    show: true,
+                    showMinLabel: true,
+                    showMaxLabel: true,
+                },
+                // 網格線
+                splitLine: {
+                    show: false,
+                },
+                min: 495 * (-1 * Data.length),
+                max: 300,
+            },
+            series: series1,
+            grid: grid,
+            dataZoom: [
+                {
+                    type: 'inside',
+                    xAxisIndex: count_channel,
+                    zoomOnMouseWheel: 'alt'
+                },
+                {
+                    type: 'slider',
+                    show: true,
+                }
+            ],
+            toolbox: toolbox,
+            brush: brush,
+        }
+        myChart.setOption(option);
 
     }
 
 
+    // Loading 
     function showLoading () {
         $q.loading.show({
             message: 'Get Data information. Please wait...',
@@ -362,9 +440,10 @@ export default function Setoption (elementID, start, end, montage) {
     }
 
     return {
-        setOption,
-        showLoading,
-        A1_A2
+        showLoading, axioss,
+        show_option,
+        dynamic_10sdata
+
 
     }
 
--- src/pages/new.vue
+++ src/pages/new.vue
@@ -24,7 +24,11 @@ export default {
     setup () {
         const start_time = ref(0);
         const end_time = ref(10);
-        const montage_type = ref(1);
+        const montage_type = ref(2);
+        const page = ref(1)
+        const Minpage = ref(1)
+        const Maxpage = ref(0)
+        const jump_to = ref(null)
         const json_url =
             "http://127.0.0.1:8000/api/v1/eegData?start_time=" +
             start_time.value +
@@ -33,16 +37,67 @@ export default {
             "&montage_type=" +
             montage_type.value;
 
+        const time_url = 'http://127.0.0.1:8000/api/v1/Time_Information'
 
         console.log(json_url)
         // const json_url = 'https://cdn.jsdelivr.net/gh/apache/echarts-website@asf-site/examples/data/asset/data/aqi-beijing.json'
-        const { setOption, A1_A2, showLoading } = option("Echarts", start_time.value, end_time.value, montage_type.value)
+        const { axioss, show_option, showLoading, dynamic_10sdata } = option("Echarts", start_time.value, end_time.value, montage_type.value)
+
+        // get 最大秒數
+        async function SecTopage (url) {
+            let api_time = await axioss(url)
+            console.log(api_time)
+            let second = Math.ceil(api_time[1] / 10)  // ceil 無條件進入
+            Maxpage.value = second
+        }
+
+        // 上一頁
+        function pre_button () {
+            page.value--
+            dynamic_10sdata(page.value, montage_type.value)
+        }
+
+        // 下一頁
+        async function next_button () {
+            page.value++
+            dynamic_10sdata(page.value, montage_type.value)
+        }
+
+        function send_page () {
+            // console.log(jump_to.value)
+            if (jump_to.value != 0) {
+                page.value = Number(jump_to.value)
+                // dynamic_10sdata('next')
+                if (isNaN(Number(jump_to.value))) {
+                    // 非數字
+                    alert('Please check input, Input type is [Number]')
+                } else if (Number(jump_to.value) > Maxpage.value) {
+                    // 大於最大頁數
+                    alert('Your page greater than ' + Maxpage.value + ' ,Please ctry again')
+                } else if ((Number(jump_to.value) < 1)) {
+                    alert('Page number is not found , Please try again')
+                } else {
+                    // dynamic_10sdata()
+                    dynamic_10sdata(page.value, montage_type.value)
+                }
+            } else {
+                alert('Page number ' + "'" + jump_to.value + "'" + '  is not found , Please try again!')
+            }
+        }
+
 
         showLoading()
-        onMounted(() => {
-            A1_A2(json_url)
-            // setOption(json_url)
+        onBeforeMount(() => {
+            SecTopage(time_url)
         })
+        onMounted(() => {
+            show_option(json_url, start_time.value, end_time.value)
+        })
+
+        return {
+            page, Minpage, Maxpage,
+            next_button, pre_button, send_page, jump_to
+        }
     },
 }
 </script>
